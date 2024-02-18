061a0b912031ab5021843ce557ce4dfdf2fba7a0
Nikhil-69 <83407509+Nikhil-69@users.noreply.github.com>
2024-02-18 17:34:52 +0530

    added metro colour

--- metro.cpp
+++ metro.cpp
@@ -1,7 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void getMetroData(vector<vector<pair<int, double > > > &metroGraph, vector<string> &stations)
+class station{
+    string name;
+    int id;
+    string color;
+    public:
+    station(string name, int id,string colour=""){
+        this->name=name;
+        this->id=id;
+    }
+    string get_name(){
+        return name;
+    }
+    int get_id(){
+        return id;
+    }
+    string get_color(){
+        return color;
+    }
+    void set_color(string color){
+        this->color=color;
+    }
+    void set_name(string name){
+        this->name=name;
+    }
+    void set_id(int id){
+        this->id=id;
+    }
+};
+
+void getMetroData(vector<vector<pair<int, double > > > &metroGraph, vector<station> &stations)
 {
     ifstream file("./metro_database.csv");
     if (!file.is_open()) {
@@ -21,7 +50,12 @@ void getMetroData(vector<vector<pair<int, double > > > &metroGraph, vector<strin
         {
             wordNumber++;
             if(wordNumber==1){
-                stations.push_back(word);
+                station temp(word, stations.size());
+                stations.push_back(temp);
+                continue;
+            }
+            if(wordNumber==2){
+                stations[stations.size()-1].set_color(word);
                 continue;
             }
             else if(wordNumber==3 || wordNumber==4){
@@ -42,11 +76,11 @@ void getMetroData(vector<vector<pair<int, double > > > &metroGraph, vector<strin
     file.close();
 }
 
-void printStations(vector<string> &stations)
+void printStations(vector<station> &stations)
 {
     for (int i = 0; i < stations.size(); i++)
     {
-        cout << i << ". " << stations[i] << endl;
+        cout << stations[i].get_id() << ". " << stations[i].get_name() << " - " << stations[i].get_color() << endl;
     }
 }
 
@@ -93,7 +127,7 @@ double shortestPath(vector<vector<pair<int, double> > > &metroGraph, int startSt
     return distance[endStation];
 }
 
-void printPath(vector<int> &path, vector<string> &stations, double distance)
+void printPath(vector<int> &path, vector<station> &stations, double distance)
 {
     cout<<"\n\nShortest Path: \n\n";
     cout<<"Number of stations: "<<path.size()<<endl;
@@ -101,13 +135,13 @@ void printPath(vector<int> &path, vector<string> &stations, double distance)
     cout<<"Path: ";
     for (int i = path.size() - 1; i >= 0; i--)
     {
-        cout << stations[path[i]] << " -> ";
+        cout << stations[path[i]].get_name() << " -> ";
     }
 }
 
 int main()
 {
-    vector<string> stations;
+    vector<station> stations;
     vector<vector<pair<int, double> > > metroGraph;
     getMetroData(metroGraph, stations);
     printStations(stations);
